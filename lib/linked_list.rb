31640f711e6a56e247d08a4fdd35dcad7f4b2826
Jonathon <jwbialas@gmail.com>
2022-11-14 20:56:56 -0600

    done

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,30 @@
 require_relative './node'
 
 class LinkedList
+    attr_accessor :head
 
+    def initialize
+      @head = nil
+    end
+
+    def prepend(node)
+        if head.nil?
+            self.head = node
+            return
+          end
+    end
+
+    def append(node)
+        if head.nil?
+            self.head = node
+            return
+          end
+
+        last_node = head
+        while last_node.next_node
+           last_node = last_node.next_node
+        end
+
+        last_node.next_node = node
+    end
 end
